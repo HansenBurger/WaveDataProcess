c0a072b7db9fc38fe6cf68a0393a02d0748b3845
HansenBurger <1035376945@qq.com>
2022-09-06 12:37:47 +0800

    change the plot showing

--- _test.py
+++ _test.py
@@ -1,9 +1,11 @@
 import BinImport
+import seaborn as sns
 from pathlib import Path
 from matplotlib import pyplot as plt
 
-data_f = Path(r'C:\Main\Data\_\Baguan\Records\202104\ZP0EBDC521040600YAR')
-f_id = 'ZP0EBDC521040600YAR_252'
+data_f = Path(
+    r'C:\Main\Data\_\WaveData\Extube\Records\202011\ZD883B2D20102600G3H')
+f_id = 'ZD883B2D20102600G3H_212'
 
 # zdt, zpx, zif all in the data_f
 
@@ -24,10 +26,22 @@ F = wave['s_F']
 V = wave['s_V']
 P = wave['s_P']
 
-plt.subplot(3, 1, 1)
-plt.plot(P[0:500])
-plt.subplot(3, 1, 2)
-plt.plot(F[0:500])
-plt.subplot(3, 1, 3)
-plt.plot(V[0:500])
+sns.set_style('white')
+fig, (ax_p, ax_f, ax_v) = plt.subplots(3, 1, figsize=(12, 6))
+ax_p.plot(P[180:620], '-', lw=5)
+ax_p.set_ylabel('P', fontdict=dict(size=17), rotation=0)
+ax_p.set_yticklabels([])
+ax_p.set_xticklabels([])
+ax_f.plot(F[180:620], '-', lw=5)
+ax_f.set_ylabel('F', fontdict=dict(size=17), rotation=0)
+ax_f.set_yticklabels([])
+ax_f.set_xticklabels([])
+ax_v.plot(V[180:620], '-', lw=5)
+ax_v.set_ylabel('V', fontdict=dict(size=17), rotation=0)
+ax_v.set_yticklabels([])
+ax_v.set_xticklabels([])
+fig.tight_layout()
+sns.despine(left=True, bottom=True, right=True)
+
 plt.show()
+plt.close()
